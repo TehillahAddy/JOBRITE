5df5ceaaab2045c7fa7284ed6b043419408f93b1
TehillahAddy <addyehillah@gmail.com>
2025-01-01 13:59:08 +0000

    new

--- backend/server.js
+++ backend/server.js
@@ -4,8 +4,11 @@ const mongoose = require('mongoose');
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+// Load environment variables from .env file
+require('dotenv').config();
+
 // Connect to MongoDB
-mongoose.connect('mongodb://localhost:27017/signupDB', {
+mongoose.connect(process.env.MONGODB_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true
 }).then(() => console.log('MongoDB connected'))
