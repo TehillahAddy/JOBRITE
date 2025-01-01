aaa9df1f3371307c0619b9633e0c2702631e321f
TehillahAddy <addyehillah@gmail.com>
2025-01-01 17:58:07 +0000

    new

--- backend/index.js
+++ backend/index.js
@@ -2,14 +2,18 @@ const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 10000;
 require('dotenv').config();
 
 // Connect to MongoDB
-mongoose.connect(process.env.MONGODB_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-}).then(() => console.log('MongoDB connected'))
+const mongoURI = process.env.MONGODB_URI;
+
+if (!mongoURI) {
+  console.error('MongoDB URI is not set');
+  process.exit(1);
+}
+
+mongoose.connect(mongoURI).then(() => console.log('MongoDB connected'))
   .catch(err => console.error('MongoDB connection error:', err));
 
 app.use(cors());
